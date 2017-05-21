b7aed20ef58bbce6bcdf78769f38f6bb77cad819
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-05-21 11:14:27 -0300

    Add descendant id on method argument, so its possible to match a block inside the method

--- kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/KappuccinoMethods.kt
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/KappuccinoMethods.kt
@@ -1,15 +1,18 @@
 package br.com.concretesolutions.kappuccino
 
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
 import android.support.annotation.IdRes
+import android.support.annotation.StringRes
 
 interface KappuccinoMethods {
-    fun id(viewId: Int): Any
-    fun text(textId: Int): Any
+    fun id(@IdRes viewId: Int): Any
+    fun text(@StringRes textId: Int): Any
     fun text(text: String): Any
-    fun contentDescription(contentDescriptionId: Int): Any
+    fun contentDescription(@StringRes contentDescriptionId: Int): Any
     fun contentDescription(contentDescription: String): Any
-    fun image(imageId: Int): Any
-    fun textColor(colorId: Int): Any
+    fun image(@DrawableRes imageId: Int): Any
+    fun textColor(@ColorRes colorId: Int): Any
     fun parent(@IdRes parentId: Int = -1, func: BaseViewMatchers.() -> Unit): Any
-    fun descendant(func: BaseViewMatchers.() -> Unit): Any
+    fun descendant(@IdRes descendantId: Int = -1, func: BaseViewMatchers.() -> Unit): Any
 }
