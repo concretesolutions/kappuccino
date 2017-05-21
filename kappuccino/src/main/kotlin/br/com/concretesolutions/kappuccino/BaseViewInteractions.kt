3e439b0a9845191111026d0b625474f38d0334da
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-05-21 12:06:40 -0300

    Remove scroll from existance assertion

--- kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/BaseViewInteractions.kt
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/BaseViewInteractions.kt
@@ -1,17 +1,21 @@
 package br.com.concretesolutions.kappuccino
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.ViewAssertion
 import android.view.View
 import br.com.concretesolutions.kappuccino.extensions.scroll
+import org.hamcrest.Matcher
 
-class BaseViewInteractions(val scroll: Boolean = false, val matchers: List<org.hamcrest.Matcher<View>>) {
+class BaseViewInteractions(val scroll: Boolean = false, val matchers: List<Matcher<View>>) {
 
-    fun check(assertion: android.support.test.espresso.ViewAssertion) {
+    fun check(assertion: ViewAssertion) {
         for (matcher in matchers)
-            android.support.test.espresso.Espresso.onView(matcher).scroll(scroll).check(assertion)
+            onView(matcher).scroll(scroll).check(assertion)
     }
 
-    fun action(viewAction: android.support.test.espresso.ViewAction) {
+    fun action(viewAction: ViewAction) {
         for (matcher in matchers)
-            android.support.test.espresso.Espresso.onView(matcher).scroll(scroll).perform(viewAction)
+            onView(matcher).scroll(scroll).perform(viewAction)
     }
 }
--- kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/assertions/ExistanceAssertions.kt
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/assertions/ExistanceAssertions.kt
@@ -8,8 +8,8 @@ import br.com.concretesolutions.kappuccino.BaseViewMatchers
 object ExistanceAssertions {
 
     // Important: here the scroll must be false, otherwise espresso will try to scroll that a view that does not exist.
-    fun notExist(scroll: Boolean = false, func: BaseViewMatchers.() -> Unit) {
+    fun notExist(func: BaseViewMatchers.() -> Unit) {
         val matchList = BaseViewMatchers().apply { func() }.matchList()
-        BaseViewInteractions(scroll, matchList).check(doesNotExist())
+        BaseViewInteractions(false, matchList).check(doesNotExist())
     }
 }
