f951fc5e2f271b1f03e3325b6aa981425f385c30
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-06-04 23:02:37 -0300

    Codebeat fix

--- kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/custom/intent/IntentMatcherInteractions.kt
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/custom/intent/IntentMatcherInteractions.kt
@@ -2,23 +2,23 @@ package br.com.concretesolutions.kappuccino.custom.intent
 
 import android.support.test.espresso.intent.Intents.intended
 import android.support.test.espresso.intent.Intents.intending
-import java.lang.Exception
 import kotlin.test.fail
 
 object IntentMatcherInteractions {
     fun matchIntent(func: IntentMatcherBuilder.() -> Unit) {
         val intentMatcherObject = IntentMatcherBuilder().apply { func() }.intentMatcher()
-        val result = intentMatcherObject.result()
+        intendMatcher(intentMatcherObject)
+    }
 
+    private fun intendMatcher(intentMatcherObject: IntentMatcherBuilder) {
+        val result = intentMatcherObject.result()
         try {
             if (result == null)
                 intended(intentMatcherObject.match())
             else
                 intending(intentMatcherObject.match()).respondWith(result.build())
-        } catch (exception: Exception) {
-            if (exception is NullPointerException)
-                fail("Don't forge to call Intents.init() before and Intents.release() after")
-            exception.printStackTrace()
+        } catch (exception: NullPointerException) {
+            fail("Don't forge to call Intents.init() before and Intents.release() after")
         }
     }
 }
