a253c3c693c9689a9cfb88d82628b34d64d41ede
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-07-20 15:31:30 -0300

    Refactor code

--- kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/custom/compoundDrawable/ClickDrawableAction.kt
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/custom/compoundDrawable/ClickDrawableAction.kt
@@ -2,6 +2,7 @@ package br.com.concretesolutions.kappuccino.custom.compoundDrawable
 
 
 import android.graphics.Point
+import android.graphics.Rect
 import android.os.SystemClock.uptimeMillis
 import android.support.annotation.IntDef
 import android.support.test.espresso.UiController
@@ -54,17 +55,41 @@ class ClickDrawableAction(private @Location val drawableLocation: Int) : ViewAct
         val tv = view as TextView
         val drawableBounds = tv.compoundDrawables[drawableLocation].bounds
 
-        val clickPoint = arrayOfNulls<Point>(4)
-        clickPoint[Left.toInt()] = Point(tv.left + drawableBounds.width() / 2, (tv.pivotY + drawableBounds.height() / 2).toInt())
-        clickPoint[Top.toInt()] = Point((tv.pivotX + drawableBounds.width() / 2).toInt(), tv.top + drawableBounds.height() / 2)
-        clickPoint[Right.toInt()] = Point(tv.right + drawableBounds.width() / 2, (tv.pivotY + drawableBounds.height() / 2).toInt())
-        clickPoint[Bottom.toInt()] = Point((tv.pivotX + drawableBounds.width() / 2).toInt(), tv.bottom + drawableBounds.height() / 2)
+        val clickPoint = createClickPoints(tv, drawableBounds)
 
         if (clickAction(MotionEvent.ACTION_DOWN, tv, clickPoint))
             clickAction(MotionEvent.ACTION_UP, tv, clickPoint)
     }
 
-    private fun clickAction(action: Int, tv: TextView, clickPoint: Array<Point?>) =
-            tv.dispatchTouchEvent(MotionEvent.obtain(uptimeMillis(), uptimeMillis(), action, clickPoint[drawableLocation]!!.x.toFloat(), clickPoint[drawableLocation]!!.y.toFloat(), 0))
+    private fun createClickPoints(tv: TextView, bounds: Rect): Array<Point> {
+        val clickPoint = arrayOf<Point>()
+        clickPoint[Left.toInt()] = PointCreator(bounds).left(tv)
+        clickPoint[Right.toInt()] = PointCreator(bounds).right(tv)
+        clickPoint[Top.toInt()] = PointCreator(bounds).top(tv)
+        clickPoint[Bottom.toInt()] = PointCreator(bounds).bottom(tv)
+        return clickPoint
+    }
 
+    private fun clickAction(action: Int, tv: TextView, clickPoint: Array<Point>) =
+            tv.dispatchTouchEvent(MotionEvent.obtain(uptimeMillis(), uptimeMillis(), action, clickPoint[drawableLocation].x.toFloat(), clickPoint[drawableLocation].y.toFloat(), 0))
+
+    private class PointCreator(private val bounds: Rect) {
+
+        fun left(textView: TextView) = Point(fromWidth(textView.left), fromHeight(textView.pivotY.toInt()))
+
+        fun right(textView: TextView) = Point(fromWidth(textView.right), fromHeight(textView.pivotY.toInt()))
+
+        fun top(textView: TextView) = Point(fromWidth(textView.pivotX.toInt()), fromHeight(textView.top))
+
+        fun bottom(textView: TextView) = Point(fromWidth(textView.pivotX.toInt()), fromHeight(textView.bottom))
+
+        private fun fromWidth(fromPosition: Int): Int {
+            return fromPosition + bounds.width() / 2
+        }
+
+        private fun fromHeight(fromPosition: Int): Int {
+            return fromPosition + bounds.height() / 2
+        }
+
+    }
 }
