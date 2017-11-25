baff8bd84ac8984d8417bc0864bf2367a340894a
Robert Keazor <robertkeazor@RobertsMBP86786.fios-router.home>
2017-11-25 12:41:52 -0500

    Add Sample Test for TextView Compound Drawable matcher

---
+++ sample/src/androidTest/kotlin/br/com/concretesolutions/kappuccino/test/ManipulateTextActivityTest.kt
@@ -0,0 +1,32 @@
+package br.com.concretesolutions.kappuccino.test
+
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import br.com.concretesolutions.kappuccino.ManipulateTextActivity
+import br.com.concretesolutions.kappuccino.R
+import br.com.concretesolutions.kappuccino.actions.ClickActions.click
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
+import br.com.concretesolutions.kappuccino.matchers.drawable.DrawablePosition.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ManipulateTextActivityTest {
+    @Rule
+    @JvmField
+    var mActivityRule = ActivityTestRule<ManipulateTextActivity>(ManipulateTextActivity::class.java, false, true)
+
+    @Test
+    fun addCompoundImageTextView() {
+        click {
+            id(R.id.add_compound_images)
+        }
+        displayed {
+            id(R.id.textView)
+            textCompoundDrawable(Start(R.mipmap.ic_launcher))
+            textCompoundDrawable(Top(R.mipmap.ic_launcher_round))
+            textCompoundDrawable(End(R.drawable.ic_android))
+        }
+    }
+}
