8e73218508661c6d656cdc384efae8c2fb20512d
Alan Camargo <alan.uk.br@gmail.com>
2018-07-31 02:54:52 -0300

    Created MenuMethods

---
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/custom/menu/MenuMethods.kt
@@ -0,0 +1,54 @@
+package br.com.concretesolutions.kappuccino.custom.menu
+
+import android.support.annotation.IdRes
+import android.support.annotation.StringRes
+import android.support.test.InstrumentationRegistry.getTargetContext
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+
+fun menu(func: MenuMethods.() -> MenuMethods) = MenuMethods().apply { func() }
+
+class MenuMethods {
+
+    fun openOptionsMenu(): MenuMethods {
+        openActionBarOverflowOrOptionsMenu(getTargetContext())
+        return this
+    }
+
+    fun onItem(@StringRes itemTextId: Int, func: Interactions.() -> Interactions): MenuMethods {
+        return createInteraction(onView(withText(itemTextId)), func)
+    }
+
+    fun onItem(itemText: String, func: Interactions.() -> Interactions): MenuMethods {
+        return createInteraction(onView(withText(itemText)), func)
+    }
+
+    fun onActionBarItem(@IdRes itemId: Int,func: Interactions.() -> Interactions): MenuMethods {
+        return createInteraction(onView(withId(itemId)), func)
+    }
+
+    private fun createInteraction(viewInteraction: ViewInteraction,
+                                  func: Interactions.() -> Interactions): MenuMethods {
+        Interactions(viewInteraction).apply { func() }
+        return this
+    }
+
+    class Interactions(private val viewInteraction: ViewInteraction) {
+
+        fun click(): Interactions {
+            viewInteraction.perform(ViewActions.click())
+            return this
+        }
+
+        fun longClick(): Interactions {
+            viewInteraction.perform(ViewActions.longClick())
+            return this
+        }
+
+    }
+
+}
