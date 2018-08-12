6da0db546c51b03eb9b0bdac72376ab677b20f7f
Heitor Colangelo <heitorcolangelo@gmail.com>
2018-08-12 18:56:40 +0100

    kappuccino: Fix CompoundDrawable tests failing [Issue 100]
    Refactor methods that were matching the drawable, which was causing the
    failure. Added some documentation to explain why it was refactored.
    Also format some code

---
+++ sample/src/androidTest/kotlin/br/com/concretesolutions/kappuccino/test/CompoundDrawablesActivityTest.kt
@@ -0,0 +1,34 @@
+package br.com.concretesolutions.kappuccino.test
+
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import br.com.concretesolutions.kappuccino.actions.ClickActions.click
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
+import br.com.concretesolutions.kappuccino.matchers.drawable.DrawablePosition.*
+import br.com.concretesolutions.kappuccino.sample.CompoundDrawablesActivity
+import br.com.concretesolutions.kappuccino.sample.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CompoundDrawablesActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityRule = ActivityTestRule<CompoundDrawablesActivity>(CompoundDrawablesActivity::class.java, false, true)
+
+    @Test
+    fun addCompoundImageTextView() {
+        click {
+            id(R.id.add_compound_images_button)
+        }
+
+        displayed {
+            textCompoundDrawable(Start(R.drawable.ic_drawable_start))
+            textCompoundDrawable(Top(R.drawable.ic_drawable_top))
+            textCompoundDrawable(End(R.drawable.ic_drawable_end))
+        }
+    }
+
+}
---
+++ sample/src/main/java/br/com/concretesolutions/kappuccino/sample/CompoundDrawablesActivity.kt
@@ -0,0 +1,24 @@
+package br.com.concretesolutions.kappuccino.sample
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+
+class CompoundDrawablesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compound_drawables)
+    }
+
+    fun onBtnClick(view: View) {
+        (view as Button).setCompoundDrawablesRelativeWithIntrinsicBounds(
+                ContextCompat.getDrawable(this, R.drawable.ic_drawable_start),
+                ContextCompat.getDrawable(this, R.drawable.ic_drawable_top),
+                ContextCompat.getDrawable(this, R.drawable.ic_drawable_end),
+                null)
+    }
+
+}
