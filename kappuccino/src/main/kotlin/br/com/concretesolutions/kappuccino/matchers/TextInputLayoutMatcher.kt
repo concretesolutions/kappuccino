8dfa8234c994c5c828f62e2e442d59e4c4310e87
Alex Soares De Siqueira <alexsoaresdesiqueira@gmail.com>
2018-10-10 20:08:16 -0300

    add withTextError and hasTextError matchers for TextInputLayout

---
+++ kappuccino/src/main/kotlin/br/com/concretesolutions/kappuccino/matchers/TextInputLayoutMatcher.kt
@@ -0,0 +1,33 @@
+package br.com.concretesolutions.kappuccino.matchers
+
+import android.support.design.widget.TextInputLayout
+import android.support.test.espresso.matcher.BoundedMatcher
+import android.view.View
+import org.hamcrest.Description
+
+fun withTextError(textToCheck: String): BoundedMatcher<View, TextInputLayout> {
+    return object : BoundedMatcher<View, TextInputLayout>(TextInputLayout::class.java) {
+        override fun describeTo(description: Description) {
+            description
+                    .appendText(" expected text was: ")
+                    .appendText(textToCheck)
+        }
+
+        override fun matchesSafely(item: TextInputLayout): Boolean {
+            return item.error == textToCheck
+        }
+    }
+}
+
+fun hasTextError(): BoundedMatcher<View, TextInputLayout> {
+    return object : BoundedMatcher<View, TextInputLayout>(TextInputLayout::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText(" has text error")
+        }
+
+        override fun matchesSafely(item: TextInputLayout): Boolean {
+            return item.error != null
+        }
+
+    }
+}
